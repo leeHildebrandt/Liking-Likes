d93e60368f8577c3d5e1f3322adcfe7d3b2a18fa
Lee Hildebrandt <dlbhildebrandt@gmail.com>
2023-08-10 08:23:25 +0200

    initial commit

---
+++ Part I/script.js
@@ -0,0 +1,7 @@
+var count = 3;
+var countElement = document.querySelector("#count")
+
+function add1() {
+    count++;
+    countElement.innerText = count +" like(s)"
+}
---
+++ Part II/script.js
@@ -0,0 +1,24 @@
+// Adds 1 to user's likes
+var count = 3;
+var countElement = document.querySelector("#count1");
+
+function count1Add1() {
+    count++;
+    countElement.innerText = count +" like(s)";
+}
+
+var count2 = 3;
+var count2Element = document.querySelector("#count2");
+
+function count2Add1() {
+    count2++;
+    count2Element.innerText = count2 +" like(s)";
+}
+
+var count3 = 3;
+var count3Element = document.querySelector("#count3");
+
+function count3Add1() {
+    count3++;
+    count3Element.innerText = count3 +" like(s)";
+}
